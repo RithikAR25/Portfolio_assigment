7977d8b9e37ca51259409e7bb742c1ec150132ee
RithikAR25 <arikothanrithik@gmail.com>
2024-07-29 20:52:31 +0530

    navbar update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import styles from './Navbar.module.css'
+import React, { useState } from 'react';
+import styles from './Navbar.module.css';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
@@ -11,22 +13,42 @@ const Navbar = () => {
     });
   };
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-        <nav className={styles.Navbar}>
-            <span onClick={scrollToTop} className={styles.name}>
-              Rithik Ramachandran
-            </span>
-            <ul className={styles.nav__item}>
-                <li className={styles.nav__item}> <Link to="/">About</Link></li>
-                <li className={styles.nav__item}><Link to="/skills">Skills</Link></li>
-                <li className={styles.nav__item}><Link to="/project">Projects</Link></li>
-                <li className={styles.nav__item}><Link to="/contact">Contact</Link></li>
+      <nav className={styles.Navbar}>
+        <span onClick={scrollToTop} className={styles.name}>
+          Rithik Ramachandran
+        </span>
+        <button className={`${styles.menu__icon} ${isOpen ? styles.open : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
 
-            </ul>
-        </nav>
+        {/* Original navigation list, hidden on small screens */}
+        <ul className={styles.nav__item}>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/skills">Skills</Link></li>
+          <li><Link to="/project">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+
+        {/* Hamburger menu list, shown on small screens when menu is open */}
+        {isOpen && (
+          <ul className={styles.hamburgerMenu}>
+            <li><Link to="/" onClick={toggleMenu}>About</Link></li>
+            <li><Link to="/skills" onClick={toggleMenu}>Skills</Link></li>
+            <li><Link to="/project" onClick={toggleMenu}>Projects</Link></li>
+            <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+          </ul>
+        )}
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
