6b7f863877cbd7ab0fc399a0c6e44eb8cb8f70f9
RithikAR25 <arikothanrithik@gmail.com>
2024-07-29 18:36:57 +0530

    done

--- src/Components/Section_contact/ContactForm.jsx
+++ src/Components/Section_contact/ContactForm.jsx
@@ -1,29 +1,102 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import styles from './ContactForm.module.css';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      from_name: '',
+      email: '',
+      message: '',
+    },
+    validationSchema: Yup.object({
+      from_name: Yup.string()
+        .min(3, 'Must be at least 3 characters')
+        .matches(/^[A-Za-z]+$/, 'Must contain only letters')
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email address')
+        .matches(/^[^\d][\w-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, 'Invalid email address')
+        .required('Required'),
+      message: Yup.string().required('Required'),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      emailjs
+        .sendForm('service_4j13xbs', 'template_yliq0eq', formik.formRef.current, {
+          publicKey: 'qX9EealRbBN_hm_1s',
+        })
+        .then(
+          () => {
+            toast.success('Email sent successfully!', {
+              position: 'top-center',
+            });
+            resetForm();
+          },
+          (error) => {
+            toast.error('Failed to send email: ' + error.text, {
+              position: 'top-center',
+            });
+          }
+        );
+    },
+  });
+
   return (
-    <div className={styles.form_box}>
-      <form id="contact_form" action="#" method="POST" encType="multipart/form-data">
-        <div className="row">
-          <label className="required" htmlFor="name">Your name :</label><br />
-          <input id="name" className={styles.input} name="name" type="text" size="30" /><br />
-          <span id="name_validation" className={styles.error_message}></span>
-        </div>
-        <div className="row">
-          <label className="required" htmlFor="email">Your email :</label><br />
-          <input id="email" className={styles.input} name="email" type="text" size="30" /><br />
-          <span id="email_validation" className={styles.error_message}></span>
-        </div>
-        <div className="row">
-          <label className="required" htmlFor="message">Your message :</label><br />
-          <textarea id="message" className={styles.input} name="message" rows="7" cols="30"></textarea><br />
-          <span id="message_validation" className={styles.error_message}></span>
-        </div>
-        <input id="submit_button" className={styles.submit_button} type="submit" value="Send email" />
-      </form>
-    </div>
+    <>
+      <div className={styles.form_box}>
+        <form ref={formik.formRef} onSubmit={formik.handleSubmit}>
+          <label>Name :</label><br />
+          <input
+            className={styles.input}
+            type="text"
+            name="from_name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.from_name}
+          />
+          {formik.touched.from_name && formik.errors.from_name ? (
+            <div className={styles.error}>{formik.errors.from_name}</div>
+          ) : null}
+          <br />
+          <label>Email :</label><br />
+          <input
+            className={styles.input}
+            type="email"
+            name="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className={styles.error}>{formik.errors.email}</div>
+          ) : null}
+          <br />
+          <label>Message</label>
+          <textarea
+            className={`${styles.inputText} ${styles.input}`}
+            name="message"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.message}
+          />
+          {formik.touched.message && formik.errors.message ? (
+            <div className={styles.error}>{formik.errors.message}</div>
+          ) : null}
+          <br />
+          <input
+            className={styles.submit_button}
+            type="submit"
+            value="Send"
+          />
+        </form>
+      </div>
+      <ToastContainer />
+    </>
   );
 };
 
